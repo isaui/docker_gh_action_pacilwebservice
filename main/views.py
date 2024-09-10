52da59ac124bc2957407edec6f4f64a8eb405594
Isa Citra Buana <110759969+isaui@users.noreply.github.com>
2024-09-10 20:56:46 +0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'npm' : '2206028932',
-        'name': 'Upi',
+        'name': 'Upi (Luthfi)',
         'class': 'PBP E'
     }
 
